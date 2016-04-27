7a495737823741fd42e034e445b01a06cd04c550
Alex Saveliev <lyolik@codeminders.com>
2016-04-27 20:24:04 +0300

    test

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 ﻿package main
 
-func f(a string) {
+func foobar(a string) {
 }
 
 func main() {
-    f("а") 
-    f("б") 
-    f("ы") 
-    f("р") 
-    f("в") 
-    f("а") 
-    f("л") 
-    f("г")
+    foobar("абырвалг") 
+    foobar("б") 
+    foobar("ы") 
+    foobar("р") 
+    foobar("в") 
+    foobar("а") 
+    foobar("л") 
+    foobar("г")
 }
