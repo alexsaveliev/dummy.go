4b80a017e6dbcb725df82d81dffa4c15f27ede67
Alex Saveliev <lyolik@codeminders.com>
2016-04-27 20:28:30 +0300

    test

--- main.go
+++ main.go
@@ -1,19 +1,4 @@
 ﻿package main
 
-func foobar(a string) /* комментарий */ {    
-    /* еще
-       один
-       комментарий 
-    */ println(a)
-}
-
 func main() {
-    foobar("абырвалг") 
-    foobar("б") 
-    foobar("ы") 
-    foobar("р") 
-    foobar("в") 
-    foobar("а") 
-    foobar("л") 
-    foobar("г")
 }
